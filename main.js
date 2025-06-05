d8a12269d9e5de5c33a9a8cc7a65b87647036cb1
ssh-expitrans <sasha.expitrans@gmail.com>
2025-06-05 08:37:38 -0600

    form setup

--- main.js
+++ main.js
@@ -227,3 +227,10 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+
+
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+.push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account', '1573519');
